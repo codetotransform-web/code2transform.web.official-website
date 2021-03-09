609532b032390ca5776f88364bf0b7e44b4a367b
Dwitikrushna Behera <dkb.01.development@gmail.com>
2021-03-09 09:21:45 +0530

    resolved push issue

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // var blocks = document.getElementsByClassName('block');
 // var container = document.getElementsByClassName('container');
 // var hs = new HorizontalScroll.default({
@@ -7,6 +6,3 @@
 //     isAnimated: true,
 //     springEffect: 0.8
 // });
-=======
-VanillaCounter();
->>>>>>> a012bf602f174675a97cf686ba9c01668011488d
